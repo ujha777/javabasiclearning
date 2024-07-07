060b5c44a53cef9b95087f83b01f16fa35a200af
ujha77 <ujha777@gmail.com>
2024-07-07 20:06:41 +0530

    ATB7x_7th_July_2024_print table

---
+++ Javabasic/src/ATB_7X_JULY_07_2024/PrintTable.java
@@ -0,0 +1,15 @@
+package ATB_7X_JULY_07_2024;
+
+public class PrintTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=2;
+		for (int i = 1; i <=10; i++) {
+			
+			System.out.println(n+"x"+i+"="+n*i);
+		}
+
+	}
+
+}
