920dd36c6300131c4360a0d52973b13d4a34ea3c
ujha77 <ujha777@gmail.com>
2024-07-07 19:31:37 +0530

    ATB7x_7th_July_2024_updated

--- Javabasic/src/ATB_7X_JULY_07_2024/FizzBizz.java
+++ Javabasic/src/ATB_7X_JULY_07_2024/FizzBizz.java
@@ -1,11 +1,27 @@
 package ATB_7X_JULY_07_2024;
 
+
+
 public class FizzBizz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("HelloWorld");
+	  for (int i = 1; i <=100; i++) {
+		  if (i%3==0 && i%5==0) {
+			  System.out.println("FizzBizz");
+		}
+		  else if (i%3==0) {
+			  System.out.println("FIZZ");
+		  }
+		  else if(i%5==0) {
+			  System.out.println("Bizz");
+		  }
+		  else {
+			  System.out.println(i);
+		  }
+		
+	}
 
 	}
 
